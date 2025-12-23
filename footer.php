9c4289500f7430e3ca9a6d22ce61ccc8b4788eef
Enis <xmarks.dev@gmail.com>
2025-12-23 17:15:08 +0100

    sweept import - main styles | header | footer | page

--- footer.php
+++ footer.php
@@ -9,23 +9,97 @@
  * @package Product_Chimp
  */
 
-?>
+// ACF Variables
+$fields              = get_field('settings-footer', 'option');
+$footer_socials      = $fields['settings-footer-socials'] ?? null;
+$footer_copytext     = $fields['settings-footer__copytext'] ?? null;
+$footer_copytext_mob = $fields['settings-footer__copytext--mob'] ?? null; ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'product-chimp' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'product-chimp' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'product-chimp' ), 'product-chimp', '<a href="https://procoders.tech/">ProCoders</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+
+    <div class="container">
+        <div class="site-footer-content">
+
+            <div class="site-footer-content__branding">
+                <?php
+                the_custom_logo();
+
+                if ($footer_socials) : ?>
+                    <div class="site-footer-content__socials">
+                        <?php
+                        foreach ($footer_socials as $key => $social) :
+                            $social_icon = $social['settings-footer-social__icon'] ?? null;
+                            $social_link = $social['settings-footer-social__link'] ?? null;
+
+                            if ($social_icon && $social_link) : ?>
+                                <a href="<?= $social_link['url'] ?>"
+                                   target="<?= $social_link['target'] ?>"
+                                   aria-label="<?= $social_link['title'] ?>">
+                                    <?= sprite_svg($social_icon['ID'], 31, 30, TRUE) ?>
+                                </a>
+                            <?php
+                            endif;
+
+                        endforeach; ?>
+                    </div>
+                <?php
+                endif; ?>
+            </div>
+
+            <?php
+            $widget_class = (is_active_sidebar('footer-widget-1')
+                && is_active_sidebar('footer-widget-2')
+                && is_active_sidebar('footer-widget-3')
+                && is_active_sidebar('footer-widget-4')
+                && is_active_sidebar('footer-widget-5'))
+                ? 'site-footer-content__widgets--full'
+                : 'site-footer-content__widgets--partial'; ?>
+
+            <div class="site-footer-content__widgets <?= $widget_class ?>">
+                <?php if (is_active_sidebar('footer-widget-1'))
+                    dynamic_sidebar('footer-widget-1'); ?>
+
+                <?php if (is_active_sidebar('footer-widget-2'))
+                    dynamic_sidebar('footer-widget-2'); ?>
+
+                <?php if (is_active_sidebar('footer-widget-3'))
+                    dynamic_sidebar('footer-widget-3'); ?>
+
+                <?php if (is_active_sidebar('footer-widget-4'))
+                    dynamic_sidebar('footer-widget-4'); ?>
+
+                <?php if (is_active_sidebar('footer-widget-5'))
+                    dynamic_sidebar('footer-widget-5'); ?>
+            </div>
+        </div>
+
+        <div class="site-footer-copyright">
+            <?php
+            if ($footer_copytext) : ?>
+                <span class="site-footer-copyright__copytext site-footer-copyright__copytext--desktop">
+                    <?= '©' . ' ' . date("Y") . ' ' . $footer_copytext ?>
+                </span>
+            <?php
+            endif;
+
+            if ($footer_copytext_mob) : ?>
+                <span class="site-footer-copyright__copytext site-footer-copyright__copytext--mobile">
+                    <?= '©' . ' ' . date("Y") . ' ' . $footer_copytext_mob ?>
+                </span>
+            <?php
+            endif;
+
+            wp_nav_menu(
+                array(
+                    'theme_location'  => 'menu-5',
+                    'menu_id'         => 'copyright-menu',
+                    'container_class' => 'copyright-navigation',
+                    'depth'           => 1,
+                )
+            ); ?>
+        </div>
+    </div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 /**
  * The header for our theme
  *
- * This is the template that displays all the <head> section and everything up until <div id="content">
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
@@ -13,62 +13,70 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'product-chimp' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#primary">
+        <?php esc_html_e('Skip to content', 'product-chimp'); ?>
+    </a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php bloginfo( 'name' ); ?>
-					</a>
-				</h1>
-				<?php
-			else :
-				?>
-				<p class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<?php bloginfo( 'name' ); ?>
-					</a>
-				</p>
-				<?php
-			endif;
-			$product_chimp_description = get_bloginfo( 'description', 'display' );
-			if ( $product_chimp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description">
-					<?php echo $product_chimp_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-				</p>
-				<?php
-			endif;
-			?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header">
+        <div class="union"></div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-				<?php esc_html_e( 'Primary Menu', 'product-chimp' ); ?>
-			</button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-					'walker'         => new ST_Walker_Nav_Menu(),
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <div class="container">
+            <div class="site-branding">
+                <?php
+                the_custom_logo(); ?>
+            </div><!-- .site-branding -->
+
+            <nav id="site-navigation" class="site-navigation">
+
+                <div class="navigation__burger">
+                    <input type="checkbox" id="navBurgerController"
+                           class="navigation__burger-controller screen-reader-text">
+
+                    <label for="navBurgerController" aria-label="toggle menu">
+                        <div class="navigation__burger-bars">
+                            <span class="bar bar1"></span>
+                            <span class="bar bar2"></span>
+                            <span class="bar bar3"></span>
+                            <span class="bar bar4"></span>
+                        </div>
+                    </label>
+                </div>
+
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location'  => 'menu-1',
+                        'menu_id'         => 'primary-menu',
+                        'container_class' => 'site-navigation-primary'
+                    )
+                );
+
+                wp_nav_menu(
+                    array(
+                        'theme_location'  => 'menu-2',
+                        'menu_id'         => 'login-menu',
+                        'container_class' => 'site-navigation-login'
+                    )
+                );
+
+                wp_nav_menu(
+                    array(
+                        'theme_location'  => 'menu-3',
+                        'menu_id'         => 'getting-started-menu',
+                        'container_class' => 'site-navigation-gstarted'
+                    )
+                );
+                ?>
+            </nav><!-- #site-navigation -->
+        </div>
+    </header><!-- #masthead -->
